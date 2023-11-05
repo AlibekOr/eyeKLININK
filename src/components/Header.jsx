f31f6de93cb6889beaef60e2ae282b1e05605ef8
Ali <alibekorakbaev3132@gmail.com>
2023-11-05 20:08:26 +0500

    count

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import PricesAll from './PricesAll'
 import { NavLink } from 'react-router-dom'
 import "../style/style.scss"
 const Header = () => {
+    const { allCount } = PricesAll()
     return (
         <header className='header'>
             <NavLink className={'header__navLink'} to={'/'}>Home</NavLink>
-            <NavLink className={'header__navLink'} to={'/cart'}>Cart</NavLink>
+            <NavLink className={'header__navLink'} to={'/cart'}>Cart  пш({allCount})</NavLink>
         </header>
     )
 }
